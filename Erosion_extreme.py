069137edd65921930ef5ec8d6a516333bb428bcb
Marina Casado <143006837+marinacsado@users.noreply.github.com>
2024-09-30 10:50:52 +0100

    Update Erosion_extreme.py

--- Erosion_extreme.py
+++ Erosion_extreme.py
@@ -11,13 +11,13 @@ mass flux at the source and their eruption time. In this model, the mass
 flux above the surface 25 km away from the source is different from the
 mass flux at the source point.
 This is due to the fact we take into account the particle distribution.
-The value for the density at this point was obtained from Isabelle's project
-(capstone_project.py)
+The value for the density at this point was obtained from
+capstone_project.py file.
 
 In this model we take into account the 3 main erosion facotrs in Eruopa in
 an extreme case:
 -S_R=  Trailing Hemisphere
--Radiolysis= subsolar point
+-Radiolysis= Subsolar point
 """
 
 import numpy as np
